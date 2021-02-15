89b302ab2ddbbea9cd2fc46aea135f8c288593d0
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2021-02-15 21:52:29 +0100

    More reliable and grounded predictor method

--- src/explore_set.cpp
+++ src/explore_set.cpp
@@ -22,84 +22,95 @@ tuple<efficient_set, vector<evaluated_point>> explore_efficient_set(
   vector<evaluated_point> ridged_points;
   
   for (int objective = 0; objective < 2; objective++) {
-    // Generic helper variables
-    
-    evaluated_point previous_point;
-    evaluated_point next_point;
-    double_vector set_direction;
-    
-    bool finished = false;
-    evaluated_point current_point = starting_point;
-    
-    double adjusted_explore_set = eps_explore_set;
-    
-    while (!finished) {
-      if (previous_point.dec_space.size() == 0) {
-        // estimate direction using single-objective gradient
-        // only make a very small step in this case
-        set_direction = 0.1 * -normalize(current_gradients[objective]);
-      } else {
-        set_direction = normalize(current_point.dec_space - previous_point.dec_space);
-      }
-      
-      next_point.dec_space = current_point.dec_space + adjusted_explore_set * set_direction;
-      next_point.dec_space = ensure_boundary(next_point.dec_space, lower, upper);
-      next_point.obj_space = fn(next_point.dec_space);
-      
-      while (adjusted_explore_set > eps_explore_set && dominates(current_point.obj_space, next_point.obj_space)) {
-        adjusted_explore_set /= 2;
-        next_point.dec_space = current_point.dec_space + adjusted_explore_set * set_direction;
-        next_point.dec_space = ensure_boundary(next_point.dec_space, lower, upper);
-        next_point.obj_space = fn(next_point.dec_space);
-      }
-      
-      double_vector ref_point = next_point.obj_space;
-      // ref_point[objective] = current_point.obj_space[objective];
-      
-      auto descent_point = descent_fn(next_point, ref_point);
-      double delta = norm(descent_point.dec_space - next_point.dec_space);
-      
-      if (!dominates(descent_point.obj_space, current_point.obj_space) &&
-          adjusted_explore_set > eps_explore_set &&
-          delta >= adjusted_explore_set) {
-        adjusted_explore_set /= 2;
-        continue;
-      } else if (delta <= 0.2 * adjusted_explore_set) {
-        adjusted_explore_set *= 1.2;
-        adjusted_explore_set = min(adjusted_explore_set, max_explore_set);
-      } else {
-        adjusted_explore_set /= 1.2;
-        adjusted_explore_set = max(adjusted_explore_set, eps_explore_set);
-      }
-      
-      next_point = descent_point;
-      
-      if (next_point.obj_space[objective] < current_point.obj_space[objective] &&
-          !strictly_dominates(next_point.obj_space, current_point.obj_space)) {
-          // successfully made step in set
-          previous_point = current_point;
-        current_point = next_point;
-        
-        set.insert({current_point.obj_space[0], current_point});
-      } else {
-        if (strictly_dominates(next_point.obj_space, current_point.obj_space)) {
-          // (Presumably) found new attraction basin!
-          ridged_points.push_back(next_point);
-        } else if (next_point.obj_space[objective] >= current_point.obj_space[objective]) {
-          // (Presumably) reached single-objective optimum
-          // TODO single-objective descent
-        } else {
-          // Something else happened that is weird
-          print("Something else, WTF");
+
+    // Some helper objects
+    evaluated_point most_recent;
+    evaluated_point second_most_recent;
+    evaluated_point predicted;
+    evaluated_point corrected;
+
+    double step_size = eps_explore_set;
+
+    bool terminate = false;
+
+    while (!terminate) {
+      // If objective == 0, the most recent points are at the beginning of set
+      // If objective == 1, the most recent points are at the end of set
+
+      if (objective == 0) {
+        most_recent = (*(set.begin())).second;
+
+        if (set.size() > 1) {
+          second_most_recent = (*(++set.begin())).second;
         }
-        // Either way, we are finished here
-        finished = true;
+      } else {
+        most_recent = (*(set.rbegin())).second;
+
+        if (set.size() > 1) {
+          second_most_recent = (*(++set.rbegin())).second;
+        }
+      }
+
+      // print(step_size);
+      
+      double_vector set_direction;
+
+      if (set.size() == 1) {
+        set_direction = -normalize(current_gradients[objective]);
+      } else {
+        set_direction = normalize(most_recent.dec_space - second_most_recent.dec_space);
+      }
+
+      predicted.dec_space = ensure_boundary(most_recent.dec_space + step_size * set_direction, lower, upper);
+      predicted.obj_space = fn(predicted.dec_space);
+
+      // Check already that new predicted point is better in tracked objective
+
+      if (predicted.obj_space[objective] >= most_recent.obj_space[objective]) {
+        print("Predicted was worse than Most Recent");
+        
+        if (step_size > eps_explore_set) {
+          step_size = max(step_size / 2, eps_explore_set);
+          continue;
+        } else {
+          break;
+        }
+      }
+
+      corrected = descent_fn(predicted, predicted.obj_space);
+
+      double correction_distance = norm(predicted.dec_space - corrected.dec_space);
+      
+      double angle_to_corrected;
+      if (set.size() > 1) {
+        angle_to_corrected = angle(most_recent.dec_space - second_most_recent.dec_space,
+                                   most_recent.dec_space - corrected.dec_space);
+      } else {
+        angle_to_corrected = 180;
+      }
+
+      if (correction_distance > step_size ||
+          angle_to_corrected < 170) {
+        print("The angle to corrected was too small " + to_string(angle_to_corrected) +
+              " or correction distance too large " + to_string(correction_distance));
+        
+        if (step_size > eps_explore_set) {
+          step_size = max(step_size / 2, eps_explore_set);
+          continue;
+        }
+      }
+
+      if (dominates(corrected.obj_space, most_recent.obj_space)) {
+        ridged_points.push_back(corrected);
+        terminate = true;
+      } else {
+        set.insert({corrected.obj_space[0], corrected});
+        step_size = min(step_size * 2, max_explore_set);
       }
     }
   }
   
-  // empty vector if finished
-  // current point if gone over ridge
+  // Explored local set and all "ridged" points
   return {set, ridged_points};
 }
 
