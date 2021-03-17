2d7aa4914ef77f39d883bc278ea1ee934b2a7255
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2021-03-18 00:23:38 +0100

    Add header for shortcut

--- src/mogsa_cpp.h
+++ src/mogsa_cpp.h
@@ -6,15 +6,25 @@
 #include <map>
 
 tuple<vector<efficient_set>, vector<tuple<int, int>>> run_mogsa(
-        const optim_fn& mo_function,
-        const gradient_fn& gradient_function,
-        const corrector_fn& descent_function,
-        const explore_set_fn& explore_set_function,
-        const vector<double_vector>& starting_points,
-        const double_vector& lower_bounds,
-        const double_vector& upper_bounds,
-        double epsilon_explore_set,
-        double epsilon_initial_step_size,
-        double maximum_explore_set);
+                const optim_fn& mo_function,
+                const gradient_fn& gradient_function,
+                const corrector_fn& descent_function,
+                const explore_set_fn& explore_set_function,
+                const vector<double_vector>& starting_points,
+                const double_vector& lower_bounds,
+                const double_vector& upper_bounds,
+                double epsilon_explore_set,
+                double epsilon_initial_step_size,
+                double maximum_explore_set);
+
+tuple<vector<efficient_set>, vector<tuple<int, int>>> run_mogsa(
+                const optim_fn& mo_function,
+                const vector<double_vector>& starting_points,
+                const double_vector& lower,
+                const double_vector& upper,
+                double epsilon_gradient,
+                double epsilon_explore_set,
+                double epsilon_initial_step_size,
+                double max_explore_set);
 
 #endif
