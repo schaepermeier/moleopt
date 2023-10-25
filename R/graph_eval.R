00ebeea9ed2d2b7a8c289ee6069e367d524ca194
Lennart Schäpermeier <lennart.schaepermeier@tu-dresden.de>
2023-10-25 16:27:05 +0200

    Add set reachabilitiy function

--- R/graph_eval.R
+++ R/graph_eval.R
@@ -57,3 +57,42 @@ compute_statistics <- function(mole_trace) {
   
   stat_list
 }
+
+#' Get reachability for set containing xopt
+#'
+#' @param mole_trace 
+#' @param xopt 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+compute_set_reachability <- function(mole_trace, xopt) {
+  set_transitions <- mole_trace$transitions[apply(mole_trace$transitions, 1, function(x) all(x >= 0)),,drop=FALSE] + 1
+  nodes <- mole_trace$transitions[,2] %>% unique + 1
+  
+  distance_to_xopt <- sapply(mole_trace$sets, function(s) {
+    sqrt(min(apply(s$dec_space, 1, function(p) sum((p - xopt)**2))))
+  })
+  
+  xopt_sets <- distance_to_xopt < 1e-2
+  
+  colnames(set_transitions) <- c("from", "to")
+  
+  tbl_transitions <- tbl_graph(edges = as.data.frame(set_transitions), nodes = data.frame(name = nodes))
+  
+  weights <- (mole_trace$transitions[mole_trace$transitions[,1]==-1, 2] + 1) %>% tabulate(nbins = max(mole_trace$transitions + 1))
+  prop <- compute_reach_proportions(tbl_transitions, weights)
+  
+  accepted_sets <- which(prop > 1 / sum(weights))
+  
+  set_nd_counts <- compute_nondominated_sets(mole_trace$sets)
+  nondom_set_ids <- which(set_nd_counts[accepted_sets] > 0)
+  
+  list(
+    set_reachability = prop,
+    set_nd_counts = set_nd_counts,
+    nondom_set_ids = nondom_set_ids,
+    xopt_sets = xopt_sets
+  )
+}
